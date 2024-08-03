c7ed205e42bf4f13fc7ccaac22fbe474606b02f2
Divyanshu Singh <112515473+dsthakur402@users.noreply.github.com>
2024-08-03 23:08:09 +0530

    Time: 1 ms (100%), Space: 44.4 MB (24.11%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,11 +1,22 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        Arrays.sort(target);
-        Arrays.sort(arr);
-        for(int i = 0; i < arr.length; i++){
-            if(target[i] != arr[i]) return false;
-        }
-        return true;
+        int n = target.length, m = arr.length;
 
+        if (n != m) return false;
+
+        int[] map = new int[1001];
+
+        for (int i = 0; i < n; i++) {
+            map[target[i]]++;
+            map[arr[i]]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (map[target[i]] != 0 || map[arr[i]] != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
