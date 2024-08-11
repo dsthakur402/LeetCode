1eb49b16d5dba37dbfd27995ab303081e8a7a4ad
Divyanshu Singh <112515473+dsthakur402@users.noreply.github.com>
2024-08-11 19:00:38 +0530

    Time: 1 ms (100%), Space: 42.2 MB (90.91%) - LeetHub

--- 1691-minimum-number-of-days-to-disconnect-island/1691-minimum-number-of-days-to-disconnect-island.java
+++ 1691-minimum-number-of-days-to-disconnect-island/1691-minimum-number-of-days-to-disconnect-island.java
@@ -1,43 +1,64 @@
 class Solution {
+    int time;
+    int[][] vis;
+    int[][] low;
+    int[] d=new int[]{0,1,0,-1,0};
+    boolean arti;
     public int minDays(int[][] grid) {
-        if (countIslands(grid) != 1) return 0;
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
-                    grid[i][j] = 0;
-                    if (countIslands(grid) != 1) return 1;
-                    grid[i][j] = 1;
+        int n=grid.length;
+        int m=grid[0].length;
+        arti=false;
+        vis=new int[n][m];
+        low=new int[n][m];
+        time=1;
+        boolean hasArt=false;
+        boolean found=false;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    if(found)
+                        return 0;
+                    found=true;
+                    art(i,j,grid,-100,-100);
                 }
             }
         }
 
-        return 2;
+        if(time==1)
+            return 0;
+
+        if(time==2)
+            return 1;
+        if(arti)
+            return 1;
+        else
+            return 2;
     }
 
-    private int countIslands(int[][] grid) {
-        boolean[][] seen = new boolean[grid.length][grid[0].length];
-        int islands = 0;
+    public void art(int row,int col,int[][] grid , int parRow,int parCol){
+        grid[row][col]=0;
+        vis[row][col]=time;
+        low[row][col]=time;
+        time++;
+        int child=0;
+        for(int i=0;i<4;i++){
+            int x=d[i]+row;
+            int y=d[i+1]+col;
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1 && !seen[i][j]) {
-                    islands++;
-                    dfs(grid, i, j, seen);
-                }
+            if(x<0 || y<0 || x>=grid.length || y>=grid[0].length || (x==parRow && y==parCol) || (vis[x][y]==0 && grid[x][y]==0))
+                continue;
+            if(vis[x][y]==0){
+                child++;
+                art(x,y,grid,row,col);
+                low[row][col]=Math.min(low[row][col],low[x][y]);
+                if(low[x][y]>=vis[row][col] && (parRow!=-100 && parCol!=-100))
+                    arti=true;
+            }else{
+                low[row][col]=Math.min(low[row][col],vis[x][y]);
             }
         }
-        return islands;
-    }
 
-    private void dfs(int[][] grid, int r, int c, boolean[][] seen) {
-        seen[r][c] = true;
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        for (int[] dir : directions) {
-            int nr = r + dir[0], nc = c + dir[1];
-            if (nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && grid[nr][nc] == 1 && !seen[nr][nc]) {
-                dfs(grid, nr, nc, seen);
-            }
-        }
+        if(parRow==-100 && parCol==-100 && child>1)
+            arti=true;
     }
 }
