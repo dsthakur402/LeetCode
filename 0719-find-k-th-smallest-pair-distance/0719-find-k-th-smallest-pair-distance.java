2519058a8eba6fe90e5c3e20c39cd888b5a74ca2
Divyanshu Singh <112515473+dsthakur402@users.noreply.github.com>
2024-08-14 21:38:52 +0530

    Time: 8 ms (81.12%), Space: 44.3 MB (52.55%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -1,40 +1,30 @@
-
-import java.util.Arrays;
-
 class Solution {
-    public int smallestDistancePair(int[] numbers, int k) {
-        Arrays.sort(numbers);
-        int minDistance = 0;
-        int maxDistance = numbers[numbers.length - 1] - numbers[0];
-        
-        while (minDistance < maxDistance) {
-            int midDistance = minDistance + (maxDistance - minDistance) / 2;
-            int pairsCount = countPairsWithinDistance(numbers, midDistance);
-            
-            if (pairsCount < k) {
-                minDistance = midDistance + 1;
-            } else {
-                maxDistance = midDistance;
-            }
+    public int smallestDistancePair(int[] nums, int k) {
+       Arrays.sort(nums);
+       int low = 0;
+       int high = nums[nums.length-1]-nums[0];
+       while(low<high){
+        int mid = low +(high-low)/2;
+        int count = countPairsWithSumMid(nums,mid);
+        if(count>=k){
+          high=mid;   
+        } else {
+            low = mid+1;
         }
-        
-        return minDistance;
+       }
+       return low;    
     }
+    int countPairsWithSumMid(int[] nums, int sum){
+        int count=0;
+        int left=0;
 
-    private int countPairsWithinDistance(int[] numbers, int targetDistance) {
-        int count = 0;
-        int left = 0;
-        
-        for (int right = 1; right < numbers.length; right++) {
-            while (numbers[right] - numbers[left] > targetDistance) {
+        for(int right=1;right<nums.length;right++){
+            while(nums[right]-nums[left]>sum){
                 left++;
             }
-            count += right - left;
+            count+=right-left;  
         }
-        
         return count;
+        
     }
 }
-
-
-//https://leetcode.com/problems/find-k-th-smallest-pair-distance/submissions/1354706901/
