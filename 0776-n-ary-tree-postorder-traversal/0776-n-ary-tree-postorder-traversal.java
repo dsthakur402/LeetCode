f45f81b927fb2b85c793def48ac1ed3aa40cedf8
Divyanshu Singh <112515473+dsthakur402@users.noreply.github.com>
2024-08-26 23:28:14 +0530

    Time: 0 ms (100%), Space: 44.5 MB (83.57%) - LeetHub

--- 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.java
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.java
@@ -1,23 +1,34 @@
-class Solution {
-    public List<Integer> postorder(Node root) {
-        // If the root is null, return an empty list
-        if (root == null) return new ArrayList<>();
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
 
-        List<Integer> res = new ArrayList<>();
+    public Node() {}
 
-        // Start DFS from the root
-        dfs(root, res);
-
-        // Return the result list containing node values in post-order
-        return res;
+    public Node(int _val) {
+        val = _val;
     }
 
-    private void dfs(Node root, List<Integer> res) {
-        // Recursively call dfs for each child of the current node
-        for (Node child : root.children) {
-            dfs(child, res);
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        ArrayList<Integer> a=new ArrayList<>();
+        postorder(root,a);
+        return a;
+    }
+    void postorder(Node root,ArrayList<Integer> a){
+        if(root!=null){
+            for(int i=0;i<root.children.size();i++){
+                postorder(root.children.get(i),a);
+            }
+        a.add(root.val);
         }
-        // Append the value of the current node to the result list
-        res.add(root.val);
     }
 }
