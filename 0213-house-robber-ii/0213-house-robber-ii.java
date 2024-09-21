f2e1d0909030cd51be99b0b4f7bf6e91e058c45a
Divyanshu Singh <112515473+dsthakur402@users.noreply.github.com>
2024-09-21 18:56:16 +0530

    Time: 0 ms (100%), Space: 40.5 MB (96.46%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,33 +1,18 @@
 class Solution {
-    public int rob(int[] a) {
-
-        int n = a.length;
-        if(n==0) return 0;
-        if(n ==1) return a[0];
-        ArrayList<Integer> a1=new ArrayList<>();
-        ArrayList<Integer> a2=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(i!=0) a1.add(a[i]);
-            if(i!=n-1) a2.add(a[i]);
-        }
-        return Math.max(solve(a1),solve(a2));
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        return Math.max(getMax(nums, 0, nums.length - 2), getMax(nums, 1, nums.length - 1));        
     }
-    public int solve(ArrayList<Integer> a){
-        int n = a.size();
-        int prev1=a.get(0);
-        int prev2=0;
 
-        for(int i=1;i<n;i++){
+    private int getMax(int[] nums, int start, int end) {
+        int prevRob = 0, maxRob = 0;
 
-            int pick = a.get(i);
-            if(i>1) pick+=prev2;
-            int nopick = prev1;
-
-            int cur = Math.max(pick,nopick);
-
-            prev2=prev1;
-            prev1=cur;
+        for (int i = start; i <= end; i++) {
+            int temp = Math.max(maxRob, prevRob + nums[i]);
+            prevRob = maxRob;
+            maxRob = temp;
         }
-        return prev1;        
-    }
+
+        return maxRob;
+    }    
 }
